28c9a64a472747c67e1613632175c47c36114389
Pedro Vinicius <pedro.aquarius@hotmail.com>
2017-06-01 20:49:14 -0400

    atenticações

--- projeto FILA/Areas/Administracao/Controllers/HomeController.cs
+++ projeto FILA/Areas/Administracao/Controllers/HomeController.cs
@@ -6,11 +6,23 @@ using System.Web.Mvc;
 
 namespace Areas.Administracao.Controllers
 {
-    [Authorize]
+    //[Authorize(Roles = "Administrador")]
     public class HomeController : Controller
     {
         // GET: Administracao/Home
+        //Nesee controler apenas o ADM tem acesso
+        [Authorize(Roles = "Administrador")]
         public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Detalhes()
+        {
+          return View();
+        }
+        [Authorize(Roles = "Usuario")]
+        public ActionResult Usuario()
         {
             return View();
         }
