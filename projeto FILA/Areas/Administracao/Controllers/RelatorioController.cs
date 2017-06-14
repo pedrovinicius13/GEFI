f5dc6a0c9ad1356ef6f8157a4c64aaf7f0269bab
MARCIO ALEXANDRE DE QUEIROZ <maqw3@hotmail.com>
2017-06-14 20:07:15 -0300

    Adiciona controllers de relatorio e protótipo de filtros de serviços

---
+++ projeto FILA/Areas/Administracao/Controllers/RelatorioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeto_FILA.Areas.Administracao.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        ContextoEF contexto = new ContextoEF();
+        
+        public ActionResult TotalDeServicoPorTipo()
+        {
+            var total = contexto.Funcionarios.ToList();
+            return View();
+        }
+
+        public ActionResult TotalDeAtendimentoPorFuncionario()
+        {
+            var total = contexto.Funcionarios.ToList();
+            return View();
+        }
+    }
+}
